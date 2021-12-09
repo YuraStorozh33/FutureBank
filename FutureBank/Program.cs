a86db92391585a21e4ca830c278115761460e2fb
Юрій Сторож <storozh.yura@gmail.com>
2021-12-09 22:42:39 +0200

    Добавьте файлы проекта.

---
+++ FutureBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FutureBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
