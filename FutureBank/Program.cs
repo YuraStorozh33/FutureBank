aa77674a63760a47048fa6bde12d9381e5de9eb2
pavlo <pavlo.mudrak@probegin.com>
2021-12-10 14:06:36 +0200

    init comit

--- FutureBank/Program.cs
+++ FutureBank/Program.cs
@@ -6,7 +6,7 @@ namespace FutureBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("init commit test");
         }
     }
 }
